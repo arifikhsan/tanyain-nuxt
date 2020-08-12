5d7ac7a2f5f44380a685d0e92181de08e1295f29
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-13 06:39:56 +0700

    f

--- layouts/components/AppHeader.vue
+++ layouts/components/AppHeader.vue
@@ -92,13 +92,15 @@
             >
               Beranda
             </nuxt-link>
-            <nuxt-link v-if="$auth.loggedIn"
+            <nuxt-link
+              v-if="$auth.loggedIn"
               to="/tanya"
               class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
             >
               Buat Pertanyaan
             </nuxt-link>
-            <nuxt-link v-else
+            <nuxt-link
+              v-else
               to="/login"
               class="block px-3 py-2 mt-1 text-base font-medium text-gray-700 transition duration-150 ease-in-out rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50"
             >
