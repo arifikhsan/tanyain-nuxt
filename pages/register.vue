574b02cf054805ccdfa596a401755278b7d03f4c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-13 06:36:13 +0700

    register

--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="max-w-xs mx-auto">
-    <form @submit.prevent="login" class="mt-16">
+    <form @submit.prevent="register" class="mt-16">
       <label class="block">
+        <span class="text-gray-700">Nama</span>
+        <input
+          type="text"
+          v-model="credential.name"
+          class="block w-full mt-1 form-input"
+          placeholder="Nama lengkap"
+        />
+      </label>
+      <label class="block mt-2">
         <span class="text-gray-700">Email</span>
         <input
           type="email"
@@ -24,13 +33,13 @@
           class="block w-full px-4 py-2 text-center text-white duration-500 bg-blue-500 rounded-md hover:bg-blue-600"
           type="submit"
         >
-          Login
+          Daftar
         </button>
       </div>
     </form>
     <div class="mt-4">
-      <nuxt-link class="duration-500 hover:text-blue-500" to="/register">
-        <a>Belum punya akun?</a>
+      <nuxt-link class="duration-500 hover:text-blue-500" to="/login">
+        <a>Sudah punya akun?</a>
       </nuxt-link>
     </div>
   </div>
@@ -41,18 +50,21 @@ export default {
   data() {
     return {
       credential: {
-        email: 'admin@admin.com',
+        name: 'a',
+        email: 'c@a.a',
         password: '123456',
       },
     }
   },
   methods: {
-    async login() {
-      const res = await this.$auth.loginWith('local', { data: this.credential })
-      await this.$apolloHelpers.onLogin(res.data.access_token)
-      if (this.$auth.loggedIn) {
-        this.$router.push('/home')
-      }
+    async register() {
+      await this.$axios
+        .post('/register', this.credential)
+        .then((res) => {
+          alert('Berhasil mendaftar. Silahkan login')
+          this.$router.push('/login')
+        })
+        .catch((err) => alert(err.response.data.message))
     },
   },
 }
