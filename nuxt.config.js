bc84e0fc34f0dc3f5e5e5ec0177f69c0cad79403
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-14 08:49:54 +0700

    dicoding email

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,11 @@ export default {
         name: 'description',
         content: process.env.npm_package_description || 'Situs tanya jawab',
       },
+      {
+        hid: 'dicoding:email',
+        name: 'dicoding:email',
+        content: 'arif.ikhsanudin.developer@gmail.com',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
